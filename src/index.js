bd034fbcae07acea730f0a74a85c88524efa9fc8
Tim Flood <tflood9@gmail.com>
2021-04-06 11:55:39 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import manageTodo from './reducers/manageTodo';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import manageTodo from "./reducers/manageTodo";
+import { Provider } from "react-redux";
+import { createStore } from "redux";
 
 let store = createStore(manageTodo);
 
@@ -11,5 +11,5 @@ ReactDOM.render(
   <Provider store={store}>
     <App />
   </Provider>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
